f58c44ec04e135b47fca518de0e4e20e9863653c
Drew <drew.r.wilson@gmail.com>
2014-08-11 19:03:59 -0700

    MVP

---
+++ app.js
@@ -0,0 +1,49 @@
+var Twit = require('twit')
+var request = require('request');
+var htmlencode = require('htmlencode');
+var T = new Twit(require('./config.js'))
+
+
+function tweet() {
+  //build a tweet
+  // myTweet = 'Always Be Breeding';
+
+
+  request({'json':true,'uri':'http://breedtv.com/api/random/'}, function (error, response, body) {
+
+  if (!error && response.statusCode == 200) {
+    video = body[0];
+    console.log(video);
+
+    myTweet = htmlencode.htmlDecode(video.title) + ': http://breedtv.com/'+ video.slug;
+    console.log(myTweet); // Print the google web page.
+
+  }
+
+
+//  tweet it
+  T.post('statuses/update', { status: myTweet }, function(err, reply) {
+          if (err) {
+            console.log('error:', err);
+          }
+          else {
+            // console.log('reply:', reply);
+          }
+  });
+
+})
+
+}
+
+//tweet once when the script starts
+tweet()
+
+//every 4 hours, tweet again
+setInterval(function () {
+  try {
+    tweet();
+  }
+  catch (e) {
+    console.log(e);
+  }
+}, 1000 * 60 * 60 * 5);
